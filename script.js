c116e6db78f323c280c50078a45773c73158ea47
Winser Espinal <winser500@gmail.com>
2023-01-09 11:27:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    console.log();
     makeImages(res.data)
     
 })
